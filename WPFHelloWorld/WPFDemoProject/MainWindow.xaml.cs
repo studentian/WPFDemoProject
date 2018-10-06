96406079b6f464eea92850d645111ad5d27129fa
Ian Lee <L00150215@student.lyit.ie>
2018-10-06 10:47:14 +0100

    Added combobox to app

--- WPFHelloWorld/WPFDemoProject/MainWindow.xaml.cs
+++ WPFHelloWorld/WPFDemoProject/MainWindow.xaml.cs
@@ -20,15 +20,41 @@ namespace WPFDemoProject
     /// </summary>
     public partial class MainWindow : Window
     {
+        public List<Student> students = new List<Student>(); //This creates a list of students. Records are added to teh list in the MainWindow method below. 
+
         public MainWindow()
         {
             InitializeComponent();
+
+            students.Add(new Student { FirstName = "Ian", LastName = "Lee" }); //These students are in the list of students 
+            students.Add(new Student { FirstName = "John", LastName = "Smit" });
+            students.Add(new Student { FirstName = "Sally", LastName = "Jones" });
+            students.Add(new Student { FirstName = "Debbie", LastName = "Long" });
+
+            cboStudentNames.ItemsSource = students; //As it was called in the xaml file. So basically, it places the student list into the combobox. It will only show the student records though and not the names yet. 
         }
 
         private void btnClickHere_Click(object sender, RoutedEventArgs e)
         {
-            String firstName = tbxFirstName.Text.Trim();
+            string firstName = tbxFirstName.Text.Trim();
             MessageBox.Show($"Your first name is " + firstName);
+        }
+
+        public class Student //The class student is the object. Parameters within the class are the parts of the class. like first name and last name.  
+        {
+            public string FirstName { get; set; } //FirstName is not a variable. It is a parameter. Public parameters uses Pascal case. Private parameters use Camel case. 
+            public string LastName { get; set; }
+
+            public string FullName //This will enable one to pull the firstName and lastName whenever it is called. 
+            {
+                get
+                {
+                    return $"{FirstName} { LastName }";
+                }
+
+            } 
+
+
         }
     }
 }
